5cb98fd1f80a87f338b5b1560ffb0b49c5ea32df
Dheeraj CVR <cvr.dheeraj@gmail.com>
2014-04-22 11:45:35 +0530

    u2: initial commit

---
+++ u2-vendor-blobs.mk
@@ -0,0 +1,50 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/lge/u2/proprietary/etc/cert/lge.cer:system/etc/cert/lge.cer \
+    vendor/lge/u2/proprietary/etc/firmware/fw_bcmdhd.bin:system/etc/firmware/fw_bcmdhd.bin \
+    vendor/lge/u2/proprietary/etc/firmware/fw_bcmdhd_apsta.bin:system/etc/firmware/fw_bcmdhd_apsta.bin \
+    vendor/lge/u2/proprietary/etc/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin \
+    vendor/lge/u2/proprietary/etc/firmware/BCM4330B1_002.001.003.0750.0865.hcd:system/etc/firmware/BCM4330B1_002.001.003.0750.0865.hcd \
+    vendor/lge/u2/proprietary/lib/libril.so:system/lib/libril.so \
+    vendor/lge/u2/proprietary/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so \
+    vendor/lge/u2/proprietary/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
+    vendor/lge/u2/proprietary/lib/libini.so:system/lib/libini.so \
+    vendor/lge/u2/proprietary/lib/lge-ril.so:system/lib/lge-ril.so \
+    vendor/lge/u2/proprietary/lib/libhdcp.so:system/lib/libhdcp.so \
+    vendor/lge/u2/proprietary/lib/liblgehdmi.so:system/lib/liblgehdmi.so \
+    vendor/lge/u2/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/lge/u2/proprietary/vendor/lib/libPVRScopeServices_SGX540_120.so:system/vendor/lib/libPVRScopeServices_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/liblvverx.so:system/vendor/lib/liblvverx.so \
+    vendor/lge/u2/proprietary/vendor/lib/libIMGegl_SGX540_120.so:system/vendor/lib/libIMGegl_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/lge/u2/proprietary/vendor/lib/libsrv_init_SGX540_120.so:system/vendor/lib/libsrv_init_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so:system/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/libglslcompiler_SGX540_120.so:system/vendor/lib/libglslcompiler_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/hw/gralloc.omap4430.so:system/vendor/lib/hw/gralloc.omap4430.so \
+    vendor/lge/u2/proprietary/vendor/lib/libpvr2d_SGX540_120.so:system/vendor/lib/libpvr2d_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/liblvvetx.so:system/vendor/lib/liblvvetx.so \
+    vendor/lge/u2/proprietary/vendor/lib/libsmapi.so:system/vendor/lib/libsmapi.so \
+    vendor/lge/u2/proprietary/vendor/lib/libsrv_um_SGX540_120.so:system/vendor/lib/libsrv_um_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/libusc_SGX540_120.so:system/vendor/lib/libusc_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/libtf_crypto_sst.so:system/vendor/lib/libtf_crypto_sst.so \
+    vendor/lge/u2/proprietary/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so:system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    vendor/lge/u2/proprietary/vendor/bin/pvrsrvctl_SGX540_120:system/vendor/bin/pvrsrvctl_SGX540_120 \
+    vendor/lge/u2/proprietary/vendor/bin/pvrsrvinit:system/vendor/bin/pvrsrvinit \
+    vendor/lge/u2/proprietary/bin/sensord:system/bin/sensord \
+    vendor/lge/u2/proprietary/bin/glgps:system/bin/glgps \
+    vendor/lge/u2/proprietary/bin/rild:system/bin/rild
